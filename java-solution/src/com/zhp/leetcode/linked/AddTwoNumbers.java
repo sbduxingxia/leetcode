2e14a2d9515f55606e0baf5aeaea251e535b04cb
zhangpeng <11111111>
2017-11-02 14:03:46 +0800

    add two numbers

--- java-solution/src/com/zhp/leetcode/linked/AddTwoNumbers.java
+++ java-solution/src/com/zhp/leetcode/linked/AddTwoNumbers.java
@@ -8,6 +8,22 @@ import com.zhp.leetcode.linked.def.ListNode;
  */
 public class AddTwoNumbers {
     public static ListNode solution(ListNode l1, ListNode l2){
+        ListNode result=new ListNode(0),nextNode=result;
+        int addNext = 0;
+        while(l1!=null||l2!=null){
+            int sum =(l1==null?0:l1.val)+(l2==null?0:l2.val)+addNext;
+            l1 = l1!=null?l1.next:null;
+            l2 = l2!=null?l2.next:null;
+            addNext = sum/10;
+            nextNode.next = new ListNode(sum%10);
+            nextNode = nextNode.next;
+        }
+        if(addNext>0){
+            nextNode.next = new ListNode(addNext);
+        }
+        return result.next;
+    }
+    public static ListNode solution2(ListNode l1, ListNode l2){
         ListNode result=null,nextNode=null;
         int addNext = 0;
         while(l1!=null||l2!=null||addNext>0){
