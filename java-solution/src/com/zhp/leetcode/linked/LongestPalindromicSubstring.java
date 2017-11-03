c66b9db78d74623182078289b459dfc3fa9a095e
zhangpeng <11111111>
2017-11-03 14:58:47 +0800

    Longest Palindromic Substring

--- java-solution/src/com/zhp/leetcode/linked/LongestPalindromicSubstring.java
+++ java-solution/src/com/zhp/leetcode/linked/LongestPalindromicSubstring.java
@@ -9,65 +9,69 @@ import java.util.HashMap;
  */
 public class LongestPalindromicSubstring {
     public static String solution(String s){
-        if(s==null||s.length()==0) {
-            return "";
-        }
-        byte[] chars = s.getBytes();
-        int beginIndex = 0,endIndex=0,lastIndex=0;
-
-        int sameBegin = 0,sameEnd=0;
-        HashMap<Byte,Integer> char2Index = new HashMap<Byte,Integer>();
-        int maxLength = 0;
+        int beginIndex=0,endIndex=0,maxLength=0;
         int[] resultIndex = new int[2];
-        for(int i = 0 ;i<chars.length;i++){
-            byte one = chars[i];
-            if(char2Index.containsKey(one)){
-                lastIndex  = char2Index.get(one);
-                if(lastIndex+i==(i-1)*2||i-lastIndex==1){//符合第一个的情况
-                    if(i-lastIndex==1){//连续相同字符
-                        beginIndex = lastIndex;
-                        endIndex = i;
-                        for(int j = i+1;j<chars.length;j++){
-                            if(chars[j]==chars[i]){
-                                beginIndex = lastIndex;
-                                endIndex = j;
-                            }else{
-                                break;
-                            }
-                        }
-                        if(endIndex-beginIndex>maxLength){
-                            maxLength = endIndex-beginIndex;
-                            resultIndex[0] = beginIndex;
-                            resultIndex[1] = endIndex;
-                        }
-                    }
-                    beginIndex = lastIndex;
-                    endIndex = i;
-                    for(int j = i+1;j<chars.length;j++){
-                        if(beginIndex-1<0){
-                            break;
-                        }
-                        if(chars[beginIndex-1]==chars[j]){
-                            beginIndex = beginIndex-1;
-                            endIndex++;
-                        }else{
-                            break;
-                        }
-                    }
-                    if(endIndex-beginIndex>maxLength){
-                        maxLength = endIndex-beginIndex;
-                        resultIndex[0] = beginIndex;
-                        resultIndex[1] = endIndex;
-                    }
-                }
+        for(int i=1;i<s.length();i++){
+            beginIndex = i;
+            endIndex = i;
+            while(beginIndex-1>=0&&s.charAt(beginIndex-1)==s.charAt(i)){
+                beginIndex--;
             }
-            char2Index.put(one,i);
+            while(endIndex+1<s.length()&&s.charAt(endIndex+1)==s.charAt(i)){
+                endIndex++;
+            }
+            i=endIndex;
+            while(beginIndex-1>=0&&endIndex+1<s.length()&&s.charAt(beginIndex-1)==s.charAt(endIndex+1)){
+                beginIndex--;
+                endIndex++;
+            }
+            int cacheLen = endIndex-beginIndex;
+            if(cacheLen%2==0){
+
+            }
+            if(maxLength<cacheLen){
+                maxLength=cacheLen;
+                resultIndex[0] = beginIndex;
+                resultIndex[1] = endIndex;
+            }
         }
         return s.substring(resultIndex[0],resultIndex[1]+1);
     }
+    public static String solution2(String s){
+        int beginIndex=0,endIndex=0,maxLength=0;
+        int[] resultIndex = new int[2];
+        for(int i=1;i<s.length();i++){
+            if(s.charAt(i-1)==s.charAt(i)){//
+                beginIndex = i-1;
+                endIndex = i;
+                while(beginIndex-1>=0&&s.charAt(beginIndex-1)==s.charAt(i)){
+                    beginIndex--;
+                }
+                while(endIndex+1<s.length()&&s.charAt(endIndex+1)==s.charAt(i)){
+                    endIndex++;
+                }
+                i=endIndex;
+            }else{
+                beginIndex =i;
+                endIndex =i;
+            }
+            while(beginIndex-1>=0&&endIndex+1<s.length()&&s.charAt(beginIndex-1)==s.charAt(endIndex+1)){
+                beginIndex--;
+                endIndex++;
+            }
+            if(maxLength<endIndex-beginIndex){
+                maxLength=endIndex-beginIndex;
+                resultIndex[0] = beginIndex;
+                resultIndex[1] = endIndex;
+            }
+        }
+        return s.substring(resultIndex[0],resultIndex[1]+1);
+    }
+
     public static void main(String[] args){
-        String s ="caabbad";//"badbadbadbadbadbadbad";//"abababababa";//
+        String s ="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";//"abababababa";//
         long startTime = System.nanoTime();
+
         System.out.println(solution(s));
         System.out.println(System.nanoTime()-startTime);
     }
