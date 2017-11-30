d688fae825d230ca43337e7fd37af6fbf6396cf7
zhangpeng <11111111>
2017-11-30 17:17:32 +0800

    Longest Valid Parentheses

--- java-solution/src/com/zhp/leetcode/linked/LongestValidParentheses.java
+++ java-solution/src/com/zhp/leetcode/linked/LongestValidParentheses.java
@@ -15,9 +15,7 @@ public class LongestValidParentheses {
         }
         int maxLength = 0;
         List<Integer> leftList = new ArrayList<>();
-
         int leftIndex = 0,rightIndex=0;
-        int cutNum = 0;
         for(int i=0;i<s.length();i++) {
             if(s.charAt(i)=='('){
                 leftList.add(i);
@@ -25,7 +23,6 @@ public class LongestValidParentheses {
             }else{
                 if(leftList.size()>0){
                     rightIndex=i;
-                    leftIndex = leftList.get(leftList.size()-1);
                     leftList.remove(leftList.size()-1);
                     if(leftList.size()>0){
                         leftIndex = leftList.get(leftList.size()-1)+1;
@@ -40,9 +37,7 @@ public class LongestValidParentheses {
                     if(rightIndex!=leftIndex){
                         maxLength = Math.max(maxLength,rightIndex-leftIndex+1);
                     }
-
                 }
-
             }
         }
         if(leftIndex!=rightIndex){
